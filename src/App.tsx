f326d5618539a0af9f799518fe53719e8af61fad
mgl <1375022007@qq.com>
2023-05-14 10:39:08 +0800

    init react-lowcode project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      low code project
+      <i className="icon iconfont icon-wenben"></i>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import {defineConfig} from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "http://template.josephxia.com",
+    },
+  },
+
+  plugins: [tsconfigPaths(), react()],
+
+  css: {
+    modules: {
+      hashPrefix: "prefix",
+    },
+
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+});
